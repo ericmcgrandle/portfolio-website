3b43cac3b5a9a1156705d29b8bac9574a47a0cea
ericmcgrandle <ericmcgrandle@gmail.com>
2020-12-12 12:39:29 -0800

    summary on hover

--- src/IndividualButton.jsx
+++ src/IndividualButton.jsx
@@ -1,4 +1,10 @@
+import { Fragment, useState } from 'react';
+
+import ShowSummary from './ShowSummary';
+
 function IndividualButton(props) {
+
+  const [summary, setSummary] = useState();
 
   const findPosition = (date) => {
     // Timeline of 4 years = 48 months --> 100% / 48months = 2.08%/month
@@ -8,8 +14,21 @@ function IndividualButton(props) {
   const position = findPosition(props.date);
 
   return (
-      <button onClick={ () => { props.handleClick(props.id, position) } } className="timeline-button" style={ { left: `${position}%` } } >
-      </button>
+    <Fragment>
+      <button 
+        onClick={ () => { props.handleClick(props.id, position) } } 
+        onMouseEnter={ () => setSummary(props.id) }
+        onMouseLeave={ () => setSummary() }
+        className="timeline-button" 
+        style={ { left: `${position}%` } } 
+      />
+      {summary && <ShowSummary 
+        title={props.title}
+        date={props.date}
+        position={position}
+      />}
+    </Fragment>
+
   )     
 }
 
---
+++ src/ShowSummary.jsx
@@ -0,0 +1,15 @@
+function ShowSummary(props) {
+  console.log('rendering showSummary');
+
+  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  const month = months[props.date.month];
+
+  return (
+    <div className="summary-event" style={ { left: `${props.position}%` } } >
+      <p className="summary-event-title">{props.title}</p>
+      <p className="summary-event-date">{month}, {props.date.year}</p>
+    </div>   
+  )     
+}
+
+export default ShowSummary;
