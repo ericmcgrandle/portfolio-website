f13abaa244a0e36df5b7994f26919a4387899c81
ericmcgrandle <ericmcgrandle@gmail.com>
2020-12-13 21:49:24 -0800

    new icons, animate icons, add href links to icons

--- src/contact/ContactMe.jsx
+++ src/contact/ContactMe.jsx
@@ -1,21 +1,43 @@
-import github from './images/github_64px.png';
-import linkedin from './images/linkedin_64px.png';
-import gmail from './images/gmail_64px.png';
+import { Fragment } from 'react';
+
+// components
+import NavBar from '../nav_bar/NavBar'
+
+import './styling/contact_me.scss'
+
+// Icons
+import github from './images/github_64px_v2.png';
+import linkedin from './images/linkedin_64px_v2.png';
+import gmail from './images/mail_64px.png';
 
 function ContactMe() {
 
   return (
-    <div className="icons">
-      <div className="github-icon">
-        <img src={github} alt="GitHub" />
+    <Fragment>
+      <NavBar />
+
+      <div className="contact-me-main">
+        <div className="icons">
+
+          <div className="icon">
+            <a href="https://github.com/ericmcgrandle" target="_blank" rel="noopener noreferrer"> <img src={github} alt="GitHub" className="img" /> </a>
+            <a href="https://github.com/ericmcgrandle" target="_blank" rel="noopener noreferrer" className="contact-text">Visit My GitHub</a>
+          </div>
+
+          <div className="icon">
+            <a href="mailto:ericmcgrandle@gmail.com" target="_blank" rel="noopener noreferrer"> <img src={gmail} alt="Gmail" /> </a>
+            <a href="mailto:ericmcgrandle@gmail.com" className="contact-text">Send Me An Email</a>
+          </div>
+
+          <div className="icon">
+            <a href="https://www.linkedin.com/in/ericmcgrandle" target="_blank" rel="noopener noreferrer"> <img src={linkedin} alt="LinkedIn" /> </a>
+            <a href="https://www.linkedin.com/in/ericmcgrandle" target="_blank" rel="noopener noreferrer" className="contact-text">Visit My LinkedIn</a>
+          </div>
+
+        </div>
       </div>
-      <div className="github-icon">
-        <img src={linkedin} alt="GitHub" />
-      </div>
-      <div className="github-icon">
-        <img src={gmail} alt="GitHub" />
-      </div>
-    </div>
+
+    </Fragment>
   )
 }
 
