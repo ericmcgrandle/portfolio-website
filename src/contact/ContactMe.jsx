632069c6f3dd4f91533abd296abd6ff5373cee4d
ericmcgrandle <ericmcgrandle@gmail.com>
2020-12-15 20:48:33 -0800

    make icons bigger

--- src/contact/ContactMe.jsx
+++ src/contact/ContactMe.jsx
@@ -6,9 +6,9 @@ import NavBar from '../nav_bar/NavBar'
 import './styling/contact_me.scss'
 
 // Icons
-import github from './images/github_64px_v2.png';
-import linkedin from './images/linkedin_64px_v2.png';
-import gmail from './images/mail_64px.png';
+import github from './images/github_128px.png';
+import linkedin from './images/linkedin_128px.png';
+import gmail from './images/mail_128px.png';
 
 function ContactMe() {
 
@@ -24,9 +24,9 @@ function ContactMe() {
             <a href="https://github.com/ericmcgrandle" target="_blank" rel="noopener noreferrer" className="contact-text">Visit My GitHub</a>
           </div>
 
-          <div className="icon">
+          <div className="icon email-icon">
             <a href="mailto:ericmcgrandle@gmail.com" target="_blank" rel="noopener noreferrer"> <img src={gmail} alt="Gmail" /> </a>
-            <a href="mailto:ericmcgrandle@gmail.com" className="contact-text">Send Me An Email</a>
+            <a href="mailto:ericmcgrandle@gmail.com" className="contact-text email-text">Send Me An Email</a>
           </div>
 
           <div className="icon">
